d60485ddcb3446df36a89f8ebed4de93cb35f4db
Mikhail Lapchinskyi <mikhaillapchinskyi@gmail.com>
2022-09-05 16:57:44 +0300

    create project

---
+++ hello_world/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    year = models.IntegerField()
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
